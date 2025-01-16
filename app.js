1d4b51fd29174daa1d36ae749ae45ba20e0e1378
Saurav Pandey <sp4398@gmail.com>
2025-01-16 21:32:40 +0530

    Refactor authentication middleware to apply to all routes and streamline admin data retrieval

--- app.js
+++ app.js
@@ -3,26 +3,25 @@ const express = require("express");
 const app = express();
 
 //Middleware
-//handle auth middleware for get routes
-app.get("/admin/getAll", (req, res) => {
-  const token = "xyz";
+//handle auth middleware for all routes
+app.use((req, res, next) => {
+  const token = "xyzfdf";
   const isAdminAuthrized = token === "xyz";
   if (isAdminAuthrized) {
-    res.send("All data sent to admin");
+    next();
   } else {
     res.status(401).send("Unauthorized");
   }
 });
 
+//handle auth middleware for get routes
+app.get("/admin/getAll", (req, res) => {
+  res.send("get all data");
+});
+
 app.get("/admin/delete", (req, res) => {
-    const token = "xyz";
-    const isAdminAuthrized = token === "xyz";
-    if (isAdminAuthrized) {
-      res.send("All data Deleted");
-    } else {
-      res.status(401).send("Unauthorized");
-    }
-  });
+  res.send("All data Deleted");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
