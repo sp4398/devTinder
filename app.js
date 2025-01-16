2c02baab8f7ccd19bd95dd6a4ea8fdeca5a44ffc
Saurav Pandey <sp4398@gmail.com>
2025-01-16 21:27:47 +0530

    Add admin routes with authorization as middlewares and nodemon support

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+//Middleware
+//handle auth middleware for get routes
+app.get("/admin/getAll", (req, res) => {
+  const token = "xyz";
+  const isAdminAuthrized = token === "xyz";
+  if (isAdminAuthrized) {
+    res.send("All data sent to admin");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
+app.get("/admin/delete", (req, res) => {
+    const token = "xyz";
+    const isAdminAuthrized = token === "xyz";
+    if (isAdminAuthrized) {
+      res.send("All data Deleted");
+    } else {
+      res.status(401).send("Unauthorized");
+    }
+  });
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
