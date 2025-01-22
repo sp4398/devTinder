353aee49410ed77c7079157b55e80f698367c163
Saurav Pandey <sp4398@gmail.com>
2025-01-22 23:13:53 +0530

    Add logout endpoint and profile editing functionality; implement validation for profile updates

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,4 +11,20 @@ const validateSignupData = (req) => {
   }
 };
 
-module.exports = { validateSignupData };
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "skills",
+    "photoUrl",
+    "about",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+  return isEditAllowed;
+};
+
+module.exports = { validateSignupData, validateEditProfileData };
