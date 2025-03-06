8d13bd037a601b01df536bc9d3f399e29fa04c43
Saurav Pandey <sp4398@gmail.com>
2025-03-06 23:57:37 +0530

    Update server port to 3000 and modify JWT secret handling in authentication middleware

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database Connected!!!");
-    app.listen(7777, () => {
+    app.listen(3000, () => {
       console.log("Server is running on port 7777...");
     });
   })
