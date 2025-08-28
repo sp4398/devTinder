746ffaa96639ae0cd731d904e70edf52088021ed
Saurav Pandey <sp4398@gmail.com>
2025-08-28 20:01:27 +0530

    fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authRouter = express.Router();
 
 const { validateSignupData } = require("../utils/validation");
-const User = require("../models/user");
+const user = require("../models/user");
 const bcrypt = require("bcrypt");
 
 authRouter.post("/signup", async (req, res) => {
