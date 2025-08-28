3a19d8bf911a0e69cc31f5d294162bf71bcc0b2d
Saurav Pandey <sp4398@gmail.com>
2025-08-28 21:29:15 +0530

    fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
