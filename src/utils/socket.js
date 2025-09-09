acabe78d1b82d80007eb039619f9e4f1e3fdcfdb
Saurav Pandey <sp4398@gmail.com>
2025-09-09 16:41:18 +0530

    feat: implement joinChat and sendMessage socket event handlers

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,9 +10,17 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     //handle socket events here
 
-    socket.on("joinChat", () => {});
+    socket.on("joinChat", ({ userId, targetedUserId }) => {
+      const roomId = [userId, targetedUserId].sort().join("_");
+      console.log(roomId);
+      socket.join(roomId);
+    });
 
-    socket.on("sendMessage", () => {});
+    socket.on("sendMessage", ({ firstName, userId, targetedUserId, text }) => {
+      const roomId = [userId, targetedUserId].sort().join("_");
+      console.log(firstName + ": " + text + " to roomId: " + roomId);
+      io.to(roomId).emit("receiveMessage", { firstName, text });
+    });
 
     socket.on("disconnect", () => {});
   });
